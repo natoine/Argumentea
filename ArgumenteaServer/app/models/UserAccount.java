064a09a2feba936d6977fb8295788100ababe6b5
antoine <antoine@choupi.(none)>
2012-10-19 15:42:48 +0200

    set the db H2 and first model class UserAccount

--- ArgumenteaServer/app/models/UserAccount.java
+++ ArgumenteaServer/app/models/UserAccount.java
@@ -1,5 +1,21 @@
 package models;
 
-public class UserAccount {
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+import play.data.validation.Constraints.Email;
+import play.data.validation.Constraints.Required;
+import play.db.ebean.Model;
+
+@Entity
+public class UserAccount extends Model
+{
+	@Id
+	public Long id;
+	
+	@Required
+	public String nickName;
+	
+	@Email @Required
+	public String mail;
 }
