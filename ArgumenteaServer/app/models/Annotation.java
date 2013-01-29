bae30932f77b507cf274889851eb505db357d01f
natoine <natoine@natoine-netbook.(none)>
2013-01-29 21:03:33 +0100

    modified annotations to be resources

--- ArgumenteaServer/app/models/Annotation.java
+++ ArgumenteaServer/app/models/Annotation.java
@@ -14,45 +14,20 @@ import com.google.code.morphia.annotations.Reference;
 import controllers.MorphiaObject;
 
 @Entity
-public class Annotation {
+public class Annotation extends Resource{
 
-	@Id
-	private ObjectId id ;
 	@Reference
 	private Article annotated ;
-	@Reference
-	private UserAccount author ;
-	@Required
-	private String content ;
 	private String annotatedContent ;
 	private String pointerBegin ; //du coup pointer mériterait d'être une classe avec sa propre méthode equals et une hierarchie de sous types
 	private String pointerEnd ;
 	
-	public ObjectId getId() {
-		return id;
-	}
-	public void setId(ObjectId id) {
-		this.id = id;
-	}
 	public Article getAnnotated() {
 		return annotated;
 	}
 	public void setAnnotated(Article annotated) {
 		this.annotated = annotated;
 	}
-	public UserAccount getAuthor() {
-		return author;
-	}
-	public void setAuthor(UserAccount author) {
-		this.author = author;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	
 	public String getAnnotatedContent() {
 		return annotatedContent;
 	}
